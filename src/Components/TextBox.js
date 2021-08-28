d05b13dbb058aa55e1898f1e629b70d3185b6c6d
the-macson <ak9991927952@gmail.com>
2021-08-28 14:48:56 +0530

    added toast

--- src/Components/TextBox.js
+++ src/Components/TextBox.js
@@ -1,53 +1,132 @@
 import { React, useState } from "react";
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
-import Button from "@material-ui/core/Button";
-import '../App.css'
-const TextBox=()=>{
-    const [text, setText] = useState("");
-    const ConvertUp =()=>{
-        let newText = text.toUpperCase();
-        setText(newText);
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../App.css";
+const TextBox = () => {
+  const [text, setText] = useState("");
+  const ConvertUp = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    toast.success("Your Text is converted into UpperCase", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const Convertlow = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    toast.success("Your Text is converted into LowerCase", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const ChangeFun = (event) => {
+    setText(event.target.value);
+  };
+  const HandleClear = () => {
+    setText("");
+    toast.success("Text is Cleared", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const HandleCopy = () => {
+    let text1 = document.getElementById("myBox");
+    text1.select();
+    navigator.clipboard.writeText(text1.value);
+    toast.success("Your Text is Copied", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const HandleRemove = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    toast.success("Extra space is Removed", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const WordsCount = () => {
+    if (text === "") {
+      return 0;
+    } else {
+      return text.endsWith(" ") === true
+        ? text.split(" ").length - 1
+        : text.split(" ").length;
     }
-    const Convertlow=()=>{
-        let newText = text.toLowerCase();
-        setText(newText);
-    }
-    const ChangeFun =(event)=>{
-        setText(event.target.value);
-    }
-    return (
-      <Container>
-        <h2>Type Your Text here for Analyzing</h2>
-        <div className="box">
-          <TextareaAutosize
-            value={text}
-            onChange={ChangeFun}
-            className="textbox"
-            aria-label="minimum height"
-            minRows={15}
-            placeholder="Enter your text here"
-          />
-        </div>
-        <span className="btn">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={ConvertUp}
-          >
-            Convert to UpperCase
-          </Button>
-        </span>
-        <span className="btn">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={Convertlow}
-          >
-            Convert to LowerCase
-          </Button>
-        </span>
-      </Container>
-    );
-}
+  };
+  return (
+    <Container>
+      <h2>Type Your Text here for Analyzing</h2>
+      <div className="box">
+        <TextareaAutosize
+          id="myBox"
+          value={text}
+          onChange={ChangeFun}
+          className="textbox"
+          aria-label="minimum height"
+          minRows={15}
+          placeholder="Enter your text here"
+        />
+      </div>
+      <button className="btn1" onClick={ConvertUp}>
+        Convert to UpperCase 
+        <ToastContainer />
+      </button>
+      <button className="btn1" onClick={Convertlow}>
+        Convert to LowerCase
+      </button>
+
+      <button className="btn1" onClick={HandleClear}>
+        Clear Text
+      </button>
+
+      <button className="btn1" onClick={HandleCopy}>
+        Copy Text
+      </button>
+
+      <button className="btn1" onClick={HandleRemove}>
+        Remove extra Space
+      </button>
+
+      <div>
+        <h2>Your Text Summary</h2>
+        <p>
+          <b>{WordsCount()}</b> words and <b>{text.length}</b> characters
+        </p>
+        <p>{0.008 * WordsCount()} Minutes read</p>
+        <h2>Privew</h2>
+        {text.length > 0 ? text : "Enter something to perview here"}
+      </div>
+    </Container>
+  );
+};
 export default TextBox;
